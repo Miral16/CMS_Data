6c354a59871d2aef893040f10ed6bda8b9f96def
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-17 16:33:51 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -183,7 +183,7 @@ Exposed = Prototype.mark_bar(opacity=.4,color='orange').encode(
 
 # Unexposed = Prototype.mark_bar(opacity=.4,color='blue').encode(
 #     alt.Tooltip(['COVID 19 Hospitalization Rate in Unexposed Population (%):Q']),
-#     y=alt.Y('COVID 19 Hospitalization Rate in Unexposed Population (%)',axis=alt.Axis(title='COVID 19 Hospitalization Rate in Unexposed Population (%)')))
+#     y=alt.Y('COVID 19 Hospitalization Rate in Unexposed Population (%):Q',axis=alt.Axis(title='COVID 19 Hospitalization Rate in Unexposed Population (%)')))
 
 # variant = Prototype.mark_line(opacity=.7,color='red',point=True).encode(
 #     tooltip = ['B.1.1.529'],
