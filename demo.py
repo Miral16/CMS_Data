f8844b2fbea30111d6997649bfc6fdbe415caad1
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-18 11:54:28 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -163,7 +163,7 @@ st.dataframe(Prototype1)
 
 # Create bar chart for Exposed hospitalization rates
 Exposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='blue').encode(
-    x=alt.X('month_order:O', axis=alt.Axis(title='month-year')),
+    x=alt.X('Month1:O', axis=alt.Axis(title='month-year')),
     y=alt.Y('COVID 19 Hospitalization Rate in Exposed Population (%):Q', axis=alt.Axis(title='COVID 19 Hospitalization Rate (%)')),
     tooltip=[alt.Tooltip('COVID 19 Hospitalization Rate in Exposed Population (%):Q')]
 )
