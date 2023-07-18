36cb013531e6d7952c291c2ab52ff1715a342a35
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-18 11:02:37 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -189,12 +189,9 @@ Unexposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='green').encode(
 )
 
 variant = alt.Chart(Prototype1).mark_line(color='red').encode(
-    x=alt.X('Month:O', axis=alt.Axis(title='month-year')),
     y=alt.Y('Variant:Q', axis=alt.Axis(title='B.1.1.529 Variant')),
     tooltip=[alt.Tooltip('Variant:Q')]
 )
-
-
 # Stack the bar charts
 layer_bars = alt.layer(Exposed, Unexposed)
 # Show the stacked bar chart
