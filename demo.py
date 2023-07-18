140ba1c4f82cee121be939b7cbed8513e07a2cc7
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-18 14:23:55 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -208,7 +208,9 @@ for trace in fig_hospitalization.data:
     fig_combined.add_trace(trace)
 
 # Configure the layout
-fig_combined.update_layout(title='COVID Hospitalization Rate & Circulating Variants Over Time', xaxis_title='Month-Year', width=1200, height=600)
+fig_combined.update_layout(title='COVID Hospitalization Rate & Circulating Variants Over Time', xaxis_title='Month-Year', width=1200, height=600,
+                           legend=dict(orientation='h', yanchor='bottom', y=-0.2, xanchor='center', x=0.5),
+                           margin=dict(l=80, r=80, t=100, b=80))
 
 # Render the Plotly figure using Streamlit
 st.plotly_chart(fig_combined)
