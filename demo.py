820fb7965c9230c0cdb49dc52d34eb4b475450dd
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-18 12:13:34 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -161,11 +161,20 @@ Prototype1 = Prototype1.drop(index=Prototype1.index[18:], inplace=False)
 
 st.dataframe(Prototype1)
 
-# Create bar chart for Exposed hospitalization rates
-Exposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='blue').encode(
+# # Create bar chart for Exposed hospitalization rates
+# Exposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='blue').encode(
+#     x=alt.X('Month:O', sort=month_order,axis=alt.Axis(title='month-year')),
+#     y=alt.Y('COVID 19 Hospitalization Rate in Exposed Population (%):Q', axis=alt.Axis(title='COVID 19 Hospitalization Rate (%)')),
+#     tooltip=[alt.Tooltip('COVID 19 Hospitalization Rate in Exposed Population (%):Q')]
+# )
+# st.altair_chart(Exposed.properties(width=1000, height=500).interactive())
+
+# # Create bar chart for Unexposed hospitalization rates
+Unexposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='green').encode(
     x=alt.X('Month:O', sort=month_order,axis=alt.Axis(title='month-year')),
-    y=alt.Y('COVID 19 Hospitalization Rate in Exposed Population (%):Q', axis=alt.Axis(title='COVID 19 Hospitalization Rate (%)')),
-    tooltip=[alt.Tooltip('COVID 19 Hospitalization Rate in Exposed Population (%):Q')]
+    y=alt.Y('COVID 19 Hospitalization Rate in Unexposed Population (%):Q', axis=alt.Axis(title='COVID 19 Hospitalization Rate (%)')),
+    tooltip=[alt.Tooltip('COVID 19 Hospitalization Rate in Unexposed Population (%):Q')]
 )
-st.altair_chart(Exposed.properties(width=1000, height=500).interactive())
+st.altair_chart(Unexposed.properties(width=1000, height=500).interactive())
+
 
