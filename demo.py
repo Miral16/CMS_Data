f0897125e0647c05ed62ef84a1024c5b295d38db
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-18 12:15:45 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -178,7 +178,7 @@ Unexposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='green').encode(
 
 # Create line chart for B.1.1.529 variant
 variant = alt.Chart(Prototype1).mark_line(color='red').encode(
-    x=alt.X('Month:O', axis=alt.Axis(title='month-year')),
+    x=alt.X('Month:O', sort=month_order,axis=alt.Axis(title='month-year')),
     y=alt.Y('Variant:Q', axis=alt.Axis(title='B.1.1.529 Variant')),
     tooltip=[alt.Tooltip('Variant:Q')]
 )
