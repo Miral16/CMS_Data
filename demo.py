b6404d0329eca24e75e4d2b7d1da21d273140f1a
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-18 11:20:15 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -173,9 +173,9 @@ fig.update_layout(
 # Display the chart using Streamlit
 st.plotly_chart(fig, use_container_width=True)
 
-month_order = ['Jan-22','Feb-22','Mar-22','Apr-22','May-22','Jun-22','Jul-22','Aug-22','Sep-22','Oct-22','Nov-22','Dec-22','Jan-23','Feb-23','Mar-23','Apr-23','May-23','Jun-23']
+# month_order = ['Jan-22','Feb-22','Mar-22','Apr-22','May-22','Jun-22','Jul-22','Aug-22','Sep-22','Oct-22','Nov-22','Dec-22','Jan-23','Feb-23','Mar-23','Apr-23','May-23','Jun-23']
 
-Prototype1['Month1'] = pd.Categorical(Prototype1['Month'], categories=month_order, ordered=True)
+# Prototype1['Month1'] = pd.Categorical(Prototype1['Month'], categories=month_order, ordered=True)
 
 # Create bar chart for Exposed hospitalization rates
 Exposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='blue').encode(
