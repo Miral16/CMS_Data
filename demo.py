78aac09216038449ad3879dc1963d75dc2d70fbf
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-18 11:27:54 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -160,6 +160,7 @@ Exposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='blue').encode(
     y=alt.Y('COVID 19 Hospitalization Rate in Exposed Population (%):Q', axis=alt.Axis(title='COVID 19 Hospitalization Rate (%)')),
     tooltip=[alt.Tooltip('COVID 19 Hospitalization Rate in Exposed Population (%):Q')]
 )
+ st.altair_chart(Exposed.properties(width=1000, height=500).interactive())
 # # Create bar chart for Unexposed hospitalization rates
 # Unexposed = alt.Chart(Prototype1).mark_bar(opacity=0.4, color='green').encode(
 #     x=alt.X('Month:O', axis=alt.Axis(title='month-year')),
