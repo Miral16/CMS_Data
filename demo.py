26eb1a633807aaebcaa809d75475256dc25433a6
Miral16 <89020426+Miral16@users.noreply.github.com>
2023-07-18 11:46:48 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -157,7 +157,7 @@ Prototype1 = pd.read_csv("Prototype1.csv")
 month_order = ["Jan-22","Feb-22","Mar-22","Apr-22","May-22","Jun-22","Jul-22","Aug-22","Sep-22","Oct-22","Nov-22","Dec-22","Jan-23","Feb-23","Mar-23","Apr-23","May-23","Jun-23"]
 
 Prototype1["Month"] = pd.Categorical(Prototype1["Month"], categories=month_order, ordered=True)
-Prototype1 = df.drop(index=Prototype1.index[18:], inplace=False)
+Prototype1 = Prototype1.drop(index=Prototype1.index[18:], inplace=False)
 
 st.dataframe(Prototype1)
 
